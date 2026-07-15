f6f0f49658ba90397c195f16a2def0e1fce54a8f
MarioCoder <444067510@qq.com>
2026-07-15 14:17:58 +0800

    fix Labels
    fix Labels

--- src/Labels.py
+++ src/Labels.py
@@ -74,5 +74,5 @@ class Labels(str, Enum):
     treasure = 'treasure'
     ult_ready = 'ult_ready'
     unlimit_gift = 'unlimit_gift'
-    boss_treasure = 'bosstreasure'
+    boss_treasure = 'boss_treasure'
     claim_page_logo = 'claim_page_logo'
